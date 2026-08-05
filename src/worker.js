fd6a9f533ec33c51dde316472c0df63bd286d3d2
SamimRahimi99 <samimrahimidolar@gmail.com>
2026-08-05 18:03:33 +0430

    Update worker.js

--- src/worker.js
+++ src/worker.js
@@ -1,5 +1,6 @@
 export default {
   async fetch(request, env) {
+
     if (request.method !== "POST") {
       return new Response("Telegram Auto Reply Bot");
     }
@@ -14,21 +15,65 @@ export default {
     const messageId = update.message.message_id;
     const text = update.message.text.trim();
 
-    // نمونه پاسخ
-    if (text === "سلام") {
-      await fetch(`https://api.telegram.org/bot${env.BOT_TOKEN}/sendMessage`, {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json"
-        },
-        body: JSON.stringify({
-          chat_id: chatId,
-          text: "علیک",
-          reply_to_message_id: messageId
-        })
-      });
+    const adminId = "7244509871";
+
+    // پنل مدیریت
+    if (text === "/admin" && String(chatId) === adminId) {
+      await sendMessage(env, chatId,
+        "⚙️ پنل مدیریت\n\n"
+        + "1- تغییر پاسخ سلام\n"
+        + "برای تغییر بنویس:\n"
+        + "/sethello متن جدید"
+      );
+      return new Response("OK");
     }
+
+    // تغییر متن پاسخ سلام
+    if (text.startsWith("/sethello") && String(chatId) === adminId) {
+      const newText = text.replace("/sethello", "").trim();
+
+      if (newText) {
+        await env.DB.put("hello_reply", newText);
+        await sendMessage(env, chatId, "✅ پاسخ سلام تغییر کرد");
+      }
+
+      return new Response("OK");
+    }
+
+
+    // پاسخ سلام
+    if (text === "سلام") {
+
+      const reply =
+        await env.DB.get("hello_reply")
+        || "علیک";
+
+      await sendMessage(env, chatId, reply, messageId);
+    }
+
 
     return new Response("OK");
   }
+};
+
+
+async function sendMessage(env, chatId, text, replyTo = null) {
+
+  await fetch(
+    `https://api.telegram.org/bot${env.BOT_TOKEN}/sendMessage`,
+    {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify({
+        chat_id: chatId,
+        text: text,
+        ...(replyTo ? {
+          reply_to_message_id: replyTo
+        } : {})
+      })
+    }
+  );
+
 }
